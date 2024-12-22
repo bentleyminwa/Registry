76655ca1486902891554eb4699f287645b826c7f
bentleyminwa <bentleyminwa@gmail.com>
2024-12-22 09:01:01 +0300

    routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./pages/Details";
+import Home from "./pages/Home";
+import Root from "./pages/Root";
+import Search from "./pages/Search";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/details/:name",
+        element: <Details />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
