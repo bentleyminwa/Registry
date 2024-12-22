1a6fd0f8a8091e38cd1c90fa30ce1a66ca471816
bentleyminwa <bentleyminwa@gmail.com>
2024-12-22 10:28:12 +0300

    home link navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/">NPM Registry</Link>
+    </header>
+  );
 };
 
 export default Header;
