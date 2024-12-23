c085ce0724597b4e1ed4f6e86c069361e3bbc538
bentleyminwa <bentleyminwa@gmail.com>
2024-12-23 08:50:05 +0300

    search input setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">NPM Registry</Link>
+      <SearchInput />
     </header>
   );
 };
